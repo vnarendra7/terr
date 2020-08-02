3ca9e9ed00f091a945a53b114b296db4342af1a8
kaza514 <63160591+kaza514@users.noreply.github.com>
2020-08-02 17:45:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider  "aws" {
-   region = "${var.aws_region}"
+   region = var.aws_region
 }
 
 resource "aws_instance" "testinstance"{
 #   count = 3
-   ami = "${var.ami}"
-   instance_type = "${var.instance_type}"
-   key_name = "${var.key}"
+   ami = var.ami
+   instance_type = var.instance_type
+   key_name = var.key
    user_data = "${file("${var.bootstrap_path}")}"
 
    tags = {
